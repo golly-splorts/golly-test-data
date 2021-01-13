83a1367b8ad444ee5535a8fcf273c8cc1b43aff5
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-01-12 23:09:16 -0800

    add butchers color replacement script

---
+++ scripts/replace_butchers_color.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsed -i -e 's/990000/BB0000/g' season0/*.json
+gsed -i -e 's/990000/BB0000/g' season1/*.json
+gsed -i -e 's/990000/BB0000/g' season2/*.json
+gsed -i -e 's/990000/BB0000/g' season3/*.json
+gsed -i -e 's/990000/BB0000/g' season4/*.json
